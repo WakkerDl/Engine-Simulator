0baf9e58615327bbbbf71589153da1486dbd3a7b
WakkerDl <160781623+WakkerDl@users.noreply.github.com>
2024-11-01 15:13:42 +0300

    Add files via upload

--- Engine/Crankshaft.py
+++ Engine/Crankshaft.py
@@ -1,12 +1,11 @@
 import Starter
 
 class Crankshaft:
-    def __init__(self, flywheel_mass, rodJournals, mass, rpm):
-        self.flywheel_mass = flywheel_mass #Масса маховика
-        self.rodJournals = rodJournals # Вкладыши 
-        self.mass = mass # Масса коленвала
+    def __init__(self, rpm):
         # rpm зависит от стартера 
         self.rpm = rpm 
-        self.pos_x = 0
-        self.pos_y = 0
+
+    def Rotationfromstarter():
+        Starter.toggle
+       
 
--- Engine/Starter.py
+++ Engine/Starter.py
@@ -5,42 +5,70 @@ import keyboard
 
 
 class Starter:
-    def __init__(self, rpm):
-        self.voltage = Const.voltage_car_system 
-        self.rpm = rpm
+
+    def __init__(self):
+        # self.voltage = Const.voltage_car_system 
+        self.rpm = 0
+        self.rpm_speed = 1
 
 
-
-def StarterEnable():
-    Starter.rpm = 0
-    while True:
+    def toggle(self):
         if keyboard.is_pressed('s'):
-            if Starter.rpm < 200:  
-                Starter.rpm += 1 
-            
+            if self.rpm < 200:  
+                self.rpm += self.rpm_speed 
 
-    
+    def get_rpm(self):
+        return self.rpm
 
+    def set_rpm_speed(self, speed):
+        self.rpm_speed =  speed
 
 
 
 if __name__ == "__main__":
     import pygame
-    import Starter
     import sys
     
 
     pygame.init()
     screen_width, screen_height = 800,400
     screen = pygame.display.set_mode((screen_width,screen_height))
+    starter = Starter()
+    starter2 = Starter()
+    starter2.set_rpm_speed(2)
     
-    starter = Starter.rpm(0)
-
-    starter.rpm 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            
+            starter.toggle()
+            starter2.toggle()
+            print("rpm1:", starter.get_rpm(), "rpm2:", starter2.get_rpm())
+            
             pygame.display.flip() 
+
+
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+    
+#     def get_info(self):
+#         return (self.name, self.age)
+
+
+
+# alex = Person(name="Alex", age=18)
+#         # Person("Alex", 18)
+# mike = Person(name='Mike', age=12)
+
+# print(alex.get_info())
+# print(mike.get_info())
+
+
